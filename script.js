18433854b1fa936a723155e94c7ee76bf6218eff
xdev200 <xdev200200@gmail.com>
2021-01-13 13:16:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// welcome to event based programming
+// and callback
+
+
+// button click
+var translateButton = document.querySelector("#translate-button");
+
+translateButton.addEventListener("click", buttonClickHandler)
+
+// read input
+var translateInput = document.querySelector("#translate-input");
+
+// show output
+var translateOutput = document.querySelector("#translate-output");
+
+
+
+var url = "https://api.funtranslations.com/translate/klingon.json"
+
+
+
+function buttonClickHandler(event) {
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error occured"))
+    
+}
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
